8fdf92be790f8f6fd1f56473082fda8c2cbd8934
Eleanor Leffler <eleanor.leffler@boeing.com>
2020-06-19 08:47:45 -0500

    WinUI and UWP Solutions added.

---
+++ DragDropFileUWP/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DragDropFileUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            fillTree1();
+        }
+
+        private void fillTree1()
+        {
+            TreeViewNode node = new TreeViewNode { Content = "Root", IsExpanded = true };
+            treeView1.RootNodes.Add(node);
+        }
+
+        private void grid_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            e.DragUIOverride.Caption = "Open";
+        }
+
+        private async void grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                if (items.Count > 0)
+                {
+                    List<StorageFile> filesToOpen = new List<StorageFile>();
+                    for (int i = 0; i < items.Count; i++)
+                    {
+                        var storageFile = items[i] as StorageFile;
+                        string filePath = storageFile.Path;
+
+                        TreeViewNode node = new TreeViewNode { Content = filePath };
+                        treeView1.RootNodes[0].Children.Add(node);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# WinUI3_Problems_DragDropFile
+
+This repository contains two solutions with sample code demonstrating the drag and drop capability onto a Grid in both UWP and WinUI3 Desktop.
+
+The UWP sample code works as expected. However, the WinUI3 Desktop sample code does not allow the user to drop the file onto the Grid.
+
+For more details about the WinUI issue, go to the issue [here](** Insert link here ).
